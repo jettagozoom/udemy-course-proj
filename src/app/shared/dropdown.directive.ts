9020ab1769daaf4787f9fb563b142717aeca87ca
jettagozoom <johnmarksjr@gmail.com>
2019-04-13 15:40:55 -0600

    Finished the Directives Deep Dive section

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,16 @@
+import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appAddDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+  isOpen = false;
+
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener('click') toggle(eventData: Event) {
+    this.isOpen = !this.isOpen;
+  }
+
+}
