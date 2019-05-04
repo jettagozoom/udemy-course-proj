1c3dac1ad8538f2b45aa6ef579ce5c82e2272ff8
jettagozoom <johnmarksjr@gmail.com>
2019-05-04 14:25:23 -0600

    Final router updates

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,28 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { Recipe } from '../recipe.model';
+import {Recipe} from '../recipe.model';
 import {RecipesService} from '../recipes.service';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+    selector: 'app-recipe-detail',
+    templateUrl: './recipe-detail.component.html',
+    styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  recipe: Recipe;
+    id: number;
+    recipe: Recipe;
 
-  constructor(private recipesService: RecipesService,
-              private route: ActivatedRoute) {}
+    constructor(private recipesService: RecipesService,
+                private route: ActivatedRoute,
+                private router: Router) {
+    }
 
-  ngOnInit() {
-      this.route.params.subscribe((params: Params) => {
-          this.recipe = this.recipesService.getRecipe(+params['id']);
-      });
-  }
+    ngOnInit() {
+        this.route.params.subscribe((params: Params) => {
+            this.id = +params['id'];
+            this.recipe = this.recipesService.getRecipe(+params['id']);
+        });
+    }
 
-  onAddToShoppingList() {
-    this.recipesService.addIngredientsToShoppingList(this.recipe.ingredients);
-  }
+    onAddToShoppingList() {
+        this.recipesService.addIngredientsToShoppingList(this.recipe.ingredients);
+    }
+
+    onEditRecipe() {
+        // Programatically navigate
+        this.router.navigate(['edit'], {relativeTo: this.route});
+
+        // This form uses a more complex router, going up a level
+        // and constructing the route '../id/edit'
+        // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
+
+        // If we simple used the 'routerLink' in the template,
+        // we wouldn't have needed to store 'this.id', a create this method.
+    }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit } from '@angular/core';
 
 import {Recipe} from '../recipe.model';
 import {RecipesService} from '../recipes.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,11 +12,17 @@ import {RecipesService} from '../recipes.service';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
 
-  constructor(private recipesService: RecipesService) {
+  constructor(private recipesService: RecipesService,
+              private router: Router,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit() {
     this.recipes = this.recipesService.getRecipes();
+  }
+
+  onNewRecipe() {
+      this.router.navigate(['new'], {relativeTo: this.route});
   }
 
 }
